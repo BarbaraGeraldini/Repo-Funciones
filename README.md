7078cfee7ffbee972cf0f79f2ec4500032f4ed54
BarbaraGeraldini <b.geraldinic@gmail.com>
2024-10-07 00:17:54 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repo-Funciones JS
---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function pintar(ele, color){
+    ele.style.backgroundColor = color;
+    const text = document.getElementById('activeText').textContent = ' ¡Excelente! Ahora recarga la página para hacerlo nuevamente 💖';
+
+    }
+    
+
+
+addEventListener('DOMContentLoaded', function() {
+    const fondo = document.getElementById ('ele1');
+    const text = document.getElementById('activeText').textContent = ' ';
+
+    fondo.style.backgroundColor = 'green';
+})
